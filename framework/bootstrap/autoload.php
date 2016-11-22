537c6812b90844a25dd6ec5d9dfa5cbecbf863a2
Jack Whiting <hi@jackwhiting.co.uk>
2016-11-22 17:27:31 +0000

    autoload all plugins, herbert inspired

--- framework/bootstrap/autoload.php
+++ framework/bootstrap/autoload.php
@@ -1,27 +1,51 @@
 <?php
 
+/**
+ * Ensure this is only ran once.
+ */
+if (defined('BILLY_AUTOLOAD'))
+	return;
+
+define('BILLY_AUTOLOAD', microtime(true));
+
 if ( ! file_exists(__DIR__ . '/globals.php'))
     throw new Exception("Globals file not found.");
 
-require __DIR__ . '/globals.php';
-
-$root = base_directory();
-
-$plugin = $root . 'plugin.php';
+@require 'globals.php';
 
 $billy = new Billy\Framework\Application();
 
-$config = $billy->getPluginConfig($root);
+/**
+ * load all plugins
+ */
+$iterator = new DirectoryIterator(plugin_directory());
 
-register_activation_hook($plugin, function () use ($billy, $config, $root)
-{
+foreach ($iterator as $directory) {
+    
+	if ( ! $directory->valid() || $directory->isDot() || ! $directory->isDir())
+        continue;
+    
+    $root = $directory->getPath() . '/' . $directory->getFilename();
+   
+    if ( ! file_exists($root . '/plugin.config.php'))
+        continue;
+	
+	$config = $billy->getPluginConfig($root);
+    $plugin = substr($root . '/plugin.php', strlen(plugin_directory()));
+    $plugin = ltrim($plugin, '/');
+
+	register_activation_hook($plugin, function () use ($billy, $config, $root)
+	{
+		$billy->loadPlugin($config);
+		$billy->activatePlugin($root);
+	});
+
+	register_deactivation_hook($plugin, function () use ($billy, $root)
+	{
+		$billy->deactivatePlugin($root);
+	});
+	
+ 	@require_once $root.'/plugin.php';
+
 	$billy->loadPlugin($config);
-	$billy->activatePlugin($root);
-});
-
-register_deactivation_hook($plugin, function () use ($billy, $root)
-{
-	$billy->deactivatePlugin($root);
-});
-
-$billy->loadPlugin($config);
+}
